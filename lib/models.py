8651c1710c4e2a0a4949ad6ac8230377150f421a
Monica Gerard <mlgvla@gmail.com>
2022-12-28 00:00:42 -0500

    solution

--- lib/models.py
+++ lib/models.py
@@ -8,8 +8,72 @@ engine = create_engine('sqlite:///authors.db')
 session = sessionmaker(bind=engine)()
 
 class Author(Base):
-    pass
+    __tablename__ = 'authors'
+    
+    id = Column(Integer(), primary_key=True)
+    name = Column(String(), unique=True, nullable=False)
+    phone_number = Column(String())
+    created_at = Column(DateTime(), server_default=func.now())
+    updated_at = Column(DateTime(), onupdate=func.now())
+    
+    def __repr__(self):
+        return f'<Author id={self.id}, name={self.name}>'
+    
+    @validates('name')
+    def validate_name(self, key, name):
+        names = session.query(Author.name).all()
+        if not name:
+            raise ValueError("Name field is required")
+        elif name in names:
+            raise ValueError("Name must be unique")
+        return name
+    
+    @validates('phone_number')
+    def validates_phone_number(self, key, phone_number):
+        if len(phone_number) != 10:
+            raise ValueError("Phone number must be 10 digits.")
+        return phone_number
 
 class Post(Base):
-    pass
+    __tablename__ = 'posts'
+    
+    id = Column(Integer(), primary_key=True)
+    title = Column(String(), nullable=False)
+    content = Column(String())
+    category = Column(String())
+    summary = Column(String())
+    created_at = Column(DateTime(), server_default=func.now())
+    updated_at = Column(DateTime(), onupdate=func.now())
+    
+    def __repr__(self):
+        return f'<Post id={self.id}, title={self.title}>'
+    
+    @validates('title')
+    def validate_title(self, key, title):
+        clickbait_titles = ["Won't Belive", "Secret", "Top [number]", "Guess"]
+        if not any(substring in title for substring in clickbait_titles):
+            raise ValueError("No clickbait found")
+        return title
+    
+    @validates('content', 'summary')
+    def validate_length(self, key, string):
+        if key == 'content':
+            if len(string) < 250:
+                raise ValueError("Post content must be greater than or equal to 250 characters long.")
+            
+        if key == "summary":
+            if len(string) > 250:
+                raise ValueError("Post summary must be less than or equal to 250 characters long.")
+        return string
+    
+    @validates('category')
+    def validate_category(self, key, category):
+        if category != 'Fiction' and category != 'Non-Fiction':
+            raise ValueError("Category must be Fiction or Non-Fiction.")
+        return category
+
+    def __repr__(self):
+        return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
+    
+    
 Base.metadata.create_all(engine)
